f7606a3bdfce4992308574714eeffcc62f150322
Mircea <plmn170999@gmail.com>
2023-05-09 10:56:24 +0200

    close #3

--- roman-number/src/main/java/it/unipd/mtss/App.java
+++ roman-number/src/main/java/it/unipd/mtss/App.java
@@ -4,10 +4,6 @@
 ////////////////////////////////////////////////////////////////////
 package it.unipd.mtss;
 
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
