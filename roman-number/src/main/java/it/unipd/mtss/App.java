4a0607b5e4a988f398b046fc64aed86c578867bc
Mircea <plmn170999@gmail.com>
2023-05-09 10:54:14 +0200

    close#3

--- roman-number/src/main/java/it/unipd/mtss/App.java
+++ roman-number/src/main/java/it/unipd/mtss/App.java
@@ -1,13 +1,15 @@
+////////////////////////////////////////////////////////////////////
+// Mircea Plamadeala 2009996
+// Luca Marcato 2014720
+////////////////////////////////////////////////////////////////////
 package it.unipd.mtss;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
